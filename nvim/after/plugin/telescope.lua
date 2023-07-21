88f5aa01d4e1f76839bf43268008c591d57c50ad
jimnor0xF <xanthos444@gmail.com>
2023-07-21 21:10:20 +0200

    Show dotfiles

--- nvim/after/plugin/telescope.lua
+++ nvim/after/plugin/telescope.lua
@@ -1,3 +1,13 @@
+local telescope = require('telescope')
+  telescope.setup{
+    pickers = {
+      find_files = {
+        find_command = {'rg', '--files', '--hidden', '-g', '!.git'},
+      }
+  }
+}
+
+
 local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
 vim.keymap.set('n', '<C-p>', builtin.git_files, {})
